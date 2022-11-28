e63eca19301cc44baf6170abe4a56685c5c5055e
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2022-11-28 12:06:21 +0100

    Working tic tac toe improved

--- main.js
+++ main.js
@@ -33,18 +33,32 @@ tiles.forEach((elm) => {
 function checkForWin() {
 	let winner = false;
 	for (let i = 0; i < winRows.length; i++) {
+		/**
+		 * We pakken een winnende rij uit winRows
+		 */
 		const row = winRows[i];
 
+		/* We pakken eerst de juiste elementen
+		 * die een winnende rij vormen
+		 */
 		const x = tiles[row[0]].textContent;
 		const y = tiles[row[1]].textContent;
 		const z = tiles[row[2]].textContent;
 
+		/**
+		 * We mogen lege tegeltjes niet vergelijken,
+		 * dus slaan we de rest over om de volgende
+		 * winnende rij te pakken
+		 */
 		if (x == "" || y == "" || z == "") continue;
 
-		console.log();
+		/**
+		 * We vergelijken de 3 tegels of ze gelijk zijn
+		 * aan elkaar. Indien ja, dan hebben we een winnaar
+		 */
 		if (x === y && y === z) {
 			console.log("We have a winner!");
-            winner = true;
+			winner = true;
 			break;
 		}
 		console.log("No winner yet.");
