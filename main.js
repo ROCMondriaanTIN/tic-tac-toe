3e3936203a94bb4aedcf32657452a5e1386144fd
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2022-12-16 14:42:17 +0100

    Implemented score

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const win = new Audio("audio/won.wav");
 
 const tiles = document.querySelectorAll(".tile");
 const restart = document.querySelector(".restart");
+const scoreX = document.querySelector("#score-x");
+const scoreO = document.querySelector("#score-o");
+
 let toggle = true;
 const winRows = [
 	[0, 1, 2],
@@ -24,25 +27,31 @@ console.log(storageApi.getPlayerO());
 
 tiles.forEach((elm) => {
 	elm.addEventListener("click", (e) => {
+		// play touch sound
 		click.play();
 
+		// if tile already has X or O do nothing
 		if (elm.textContent != "") return;
 
+		// fill in X or O
 		if (toggle) {
 			elm.textContent = "X";
 		} else {
 			elm.textContent = "O";
 		}
 
+		// switch player turn
 		toggle = !toggle;
 
-		// check of een rij compleet is
+		// check if a row is complete
 		if (!checkForWin()) {
 			elm.classList.add("blinker");
 		}
 	});
 
+	// add animation end event
 	elm.addEventListener("animationend", (e) => {
+		// remove animation class
 		elm.classList.remove("blinker");
 	});
 });
@@ -51,25 +60,24 @@ function checkForWin() {
 	let winner = false;
 	for (let i = 0; i < winRows.length; i++) {
 		/**
-		 * We pakken een winnende rij uit winRows
+		 * We pick a winning row from winRows
 		 */
 		const row = winRows[i];
 
-		/* We pakken eerst de juiste elementen
-		 * die een winnende rij vormen
+		/**
+		 * We pick the right tile elements
 		 */
 		const x = [tiles[row[0]], tiles[row[1]], tiles[row[2]]];
 
 		/**
-		 * We mogen lege tegeltjes niet vergelijken,
-		 * dus slaan we de rest over om de volgende
-		 * winnende rij te pakken
+		 * We don't compare empty tiles, so we skip
+		 * the rest of the code and continue from the
+		 * beginning of the loop.
 		 */
 		if (x[0].textContent == "" || x[1].textContent == "" || x[2].textContent == "") continue;
 
 		/**
-		 * We vergelijken de 3 tegels of ze gelijk zijn
-		 * aan elkaar. Indien ja, dan hebben we een winnaar
+		 * We compare 3 tiles for the same value
 		 */
 		if (x[0].textContent === x[1].textContent && x[1].textContent === x[2].textContent) {
 			console.log("We have a winner!");
@@ -80,6 +88,10 @@ function checkForWin() {
 				tile.classList.add("blinker");
 			});
 
+			if (x[0].textContent === "X") updateScore(scoreX);
+			else updateScore(scoreO);
+
+			// if we have a winning situation, break from the loop
 			break;
 		}
 		console.log("No winner yet.");
@@ -87,6 +99,7 @@ function checkForWin() {
 
 	if (winner) {
 		console.log("Yes we have a winner");
+		// play the winning sound
 		win.play();
 	} else {
 		console.log("Nope...");
@@ -96,7 +109,13 @@ function checkForWin() {
 }
 
 restart.addEventListener("click", (e) => {
+	// clear all tiles
 	tiles.forEach((tile) => {
 		tile.textContent = "";
 	});
 });
+
+function updateScore(element) {
+	let score = Number(element.textContent);
+	element.textContent = score + 1;
+}
